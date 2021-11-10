ed47d07dfd0b49d976ff01f97d55bab3ed14606f
rebcost <rebelo.filipe020@gmail.com>
2021-11-10 15:15:49 -0400

    atualizao

---
+++ tupasys/forms.py
@@ -0,0 +1,2 @@
+class FormSetor(FlaskForm):    
+    pass
--- tupasys/routes.py
+++ tupasys/routes.py
@@ -1,14 +1,15 @@
 from tupasys import app
 from flask import render_template
 
+setores = {0: "LP-001", 1: "LP-002", 2: "LP-003", 3: "WAREHOUSE"}
 
 @app.route("/")
-def home():
-    return render_template('home.html')
+def home(): 
+    return render_template('home.html', setores=setores)
 
-@app.route("/medidor")
-def medidor():
-    return render_template('medidor.html')
+@app.route("/medidor/")
+def medidor():        
+    return render_template('medidor.html', setores=setores)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
